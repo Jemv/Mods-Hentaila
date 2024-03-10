ddd289bb1d5d32bc1a6f72f29cc37c0c1b9ada4d
TheBBC <emiliomontesv@hotmail.com>
2024-03-10 12:34:08 -0500

    files

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import json
 
-
-
-
-
-
-
 if __name__ == '__main__':
     links = []
 
-    with open('manifest.json', 'r') as f:
+    with open('./mods/manifest.json', 'r') as f:
         data = json.load(f)
         mods = data['files']
     
@@ -19,7 +13,7 @@ if __name__ == '__main__':
         link = f"https://www.curseforge.com/api/v1/mods/{project}/files/{file}/download"
         links.append(link)
     
-    with open('downloads.txt', 'w') as f:
+    with open('./apps/downloads.txt', 'w') as f:
         for link in links:
             f.write(link)
 
